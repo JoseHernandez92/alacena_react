ce954a9006cced28990786443f61c1c87c748411
Jose Hernandez <joanheru@gmail.com>
2020-09-22 09:43:39 +0200

    [APP]Added ErrorModal component

--- app/src/views/NewList.js
+++ app/src/views/NewList.js
@@ -2,12 +2,15 @@ import React, { useState } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 
 import Button from '../components/Button'
+import ErrorModal from '../components/ErrorModal'
 import Service from '../services/Service'
 
 function NewLists({ onClick, setCurrentList }) {
   const service = new Service()
-  const [list_name, setListName] = useState("")
   let history = useHistory()
+  const [list_name, setListName] = useState("")
+  const [show_error, setShowError] = useState(false)
+  const [message_error, setMessageError] = useState("")
   
   const handleInput = (event) => {
     setListName(event.target.value)
@@ -22,7 +25,9 @@ function NewLists({ onClick, setCurrentList }) {
     const response = await service.saveList(list)
     
     if(response.errorMessage){
-      return alert(response.errorMessage)
+      setMessageError(response.errorMessage)
+      setShowError(true)
+      return
     }
     onClick("Productos")
     setCurrentList(list_name)
@@ -30,10 +35,14 @@ function NewLists({ onClick, setCurrentList }) {
   }
 
   return (
+    <div>
+        <ErrorModal show={show_error} message={message_error} closeError={() => setShowError(false)}/>
     <div className="newList-container">
         <input type="text" onChange={handleInput} placeholder="¿Cómo se llama tu lista?" className="form-control"></input>
         <Button name="Crear nueva lista" onClick={handleClick} className="btn btn-light btn-lg shadow-sm"/>
         <Link to="/"><Button name="Retroceder" onClick={onClick} clickParameter="Inicio" className="btn btn-secondary btn-lg shadow-sm"/></Link>
+
+    </div>
     </div>
   )
 }
